28fc56e037fc9d6eaa2d9dea78e3b6ffa141749f
subham780 <subtech360@gmail.com>
2023-04-11 00:53:49 +0530

    commit-3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+//import Footer from "./components/Footer";
+import { 
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+  } from "react-router-dom";
+
+import Cart from "./pages/Cart";
+import { productsData } from "./api/Api";
+
+
+const Layout = () => {
+return (
+  <div>
+    <Header />
+    <Outlet />
+    <Footer />
+
+  </div>
+);
+  
+};
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    children: [
+      {
+      path: "/",
+      element: <Home />,
+      Loader: productsData,
+    },
+    {
+      path: "/cart",
+      element: <Cart />,
+    },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="font-bodyFont" >
-      <Header />
-     <Home />
+      <RouterProvider router={router}/>
+     
 
     </div>
   );
